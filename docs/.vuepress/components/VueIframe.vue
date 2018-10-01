6e8e4b120d226d66fa07da543464e236528028a4
richTheCreator <moralesrichard@live.com>
2018-10-01 12:49:54 -0500

    Exploring alt iframe loading techniques

---
+++ docs/.vuepress/components/VueIframe.vue
@@ -0,0 +1,17 @@
+<template>
+  <vue-friendly-iframe :src="'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FwZnrdYYeeRkFnrnk1xgXsh%2FComponent-Library%3Fnode-id%3D1764%253A11'"></vue-friendly-iframe>
+</template>
+<script>
+import VueFriendlyIframe from 'vue-friendly-iframe';
+
+  export default {
+    computed: {
+      title() {
+        return this.$page.title
+      }
+    },
+    components: {
+      'vue-friendly-iframe': VueFriendlyIframe
+    }
+  }
+</script>
